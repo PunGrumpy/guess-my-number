c246cd8a4989d94007410bb81025ad5a8dbcec6c
PunGrumpy <108584943+PunGrumpy@users.noreply.github.com>
2023-02-17 13:05:52 +0700

    fix(window): fix window center

--- src/main/GameWindow.java
+++ src/main/GameWindow.java
@@ -11,9 +11,9 @@ public class GameWindow {
 
     jframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     jframe.add(gamePanel);
-    // jframe.setLocationRelativeTo(null); // Centers the window
     jframe.setResizable(false); // Prevents the window from being resized
     jframe.pack(); // Adjusts the size of the window to fit the content
     jframe.setVisible(true);
+    jframe.setLocationRelativeTo(null); // Centers the window
   }
 }
