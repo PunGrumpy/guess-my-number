6577f8355e51880dd782ab79f4cb13b3cc3fb4de
PunGrumpy <108584943+PunGrumpy@users.noreply.github.com>
2023-02-25 14:45:23 +0700

    fix(panel): exit panel bug

--- src/main/Game.java
+++ src/main/Game.java
@@ -16,7 +16,7 @@ public class Game extends JFrame {
     jframe.setMaximumSize(screenSize);
     jframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     jframe.add(new GamePanel(this));
-    jframe.setAlwaysOnTop(true);
+    jframe.setAlwaysOnTop(false);
     jframe.setResizable(false); // Prevents the window from being resized
     jframe.pack(); // Adjusts the size of the window to fit the content
     jframe.setVisible(true);
