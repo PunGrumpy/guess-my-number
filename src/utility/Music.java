5c71ff4cd5447afa4ac036b479247109e1bb70ab
PunGrumpy <PunGrumpy@users.noreply.github.com>
2023-02-27 04:29:55 +0000

    🎉 code-style(prettier-java): update code style with prettier-java

--- src/utility/Music.java
+++ src/utility/Music.java
@@ -1,26 +1,32 @@
 package utility;
 
-import javax.sound.sampled.Clip;
-import javax.sound.sampled.AudioSystem;
-import javax.sound.sampled.FloatControl;
+import java.io.File;
+import java.io.IOException;
 import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.sound.sampled.FloatControl;
 import javax.sound.sampled.LineUnavailableException;
 import javax.sound.sampled.UnsupportedAudioFileException;
 
-import java.io.File;
-import java.io.IOException;
-
 public class Music {
-    public void soundEffect(String soundName) {
-        try {
-            AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(new File(soundName));
-            Clip clip = AudioSystem.getClip();
-            clip.open(audioInputStream);
-            FloatControl gainControl =  (FloatControl) clip.getControl(FloatControl.Type.MASTER_GAIN);
-            gainControl.setValue(-20.0f);
-            clip.start();
-        } catch(UnsupportedAudioFileException | LineUnavailableException | IOException e) {
-            System.out.printf("Error: %s%n", e);
-        }
+
+  public void soundEffect(String soundName) {
+    try {
+      AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(
+        new File(soundName)
+      );
+      Clip clip = AudioSystem.getClip();
+      clip.open(audioInputStream);
+      FloatControl gainControl = (FloatControl) clip.getControl(
+        FloatControl.Type.MASTER_GAIN
+      );
+      gainControl.setValue(-20.0f);
+      clip.start();
+    } catch (
+      UnsupportedAudioFileException | LineUnavailableException | IOException e
+    ) {
+      System.out.printf("Error: %s%n", e);
     }
+  }
 }
