c17f1ba4db22196baba6cbfe5311eb286d3f2d9e
PunGrumpy <108584943+PunGrumpy@users.noreply.github.com>
2023-03-17 13:43:09 +0700

    feat(random): random ranges function

--- src/utility/RandomNumber.java
+++ src/utility/RandomNumber.java
@@ -4,14 +4,20 @@ import java.security.SecureRandom;
 
 public class RandomNumber {
 
-  private int range = 50;
+  private int range;
   private SecureRandom random;
+  private int[] ranges = {1000, 10000, 100000, 1000000};
 
   public RandomNumber() {
     random = new SecureRandom();
+    range = ranges[random.nextInt(ranges.length)];
   }
 
   public int GET_RANDOM_NUMBER() {
     return random.nextInt(range) + 1;
+  }
+
+  public int GET_RANGE_NUMBER() {
+    return range;
   }
 }
