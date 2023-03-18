48a323dd657cb564a061fd822d25bfdd5b92f166
PunGrumpy <108584943+PunGrumpy@users.noreply.github.com>
2023-03-19 02:39:00 +0700

    refactor: change colorscheme class to util package

---
+++ src/utility/ColorScheme.java
@@ -0,0 +1,14 @@
+package utility;
+
+import java.awt.Color;
+
+public class ColorScheme {
+    public Color red = new Color(255, 0, 0);
+    public Color green = new Color(0, 255, 0);
+    public Color white = new Color(250, 250, 250);
+    public Color gold = new Color(227, 179, 63); // rgb(227, 179, 63)
+    public Color indigo = new Color(64, 66, 88); // rgb(64, 66, 88)
+    public Color blueGray = new Color(71, 78, 104); // rgb(71, 78, 104)
+    public Color ligthBlueGray = new Color(80, 87, 122); // rgb(80, 87, 122)
+    public Color brigthBlueGray = new Color(107, 114, 142); // rgb(107, 114, 142)
+}
