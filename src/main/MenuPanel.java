4e55aa6be600fb47dad030e9f6ffd7e243d9d700
PunGrumpy <108584943+PunGrumpy@users.noreply.github.com>
2023-03-19 02:39:18 +0700

    feat: add menu dummy

--- src/main/MenuPanel.java
+++ src/main/MenuPanel.java
@@ -1,6 +1,30 @@
 package main;
 
-public class MenuPanel {
-  // Make menu panel
-  // Run menu panel before game panel
+import javax.swing.BoxLayout;
+import javax.swing.JPanel;
+import utility.Database;
+import utility.ColorScheme;
+
+public class MenuPanel extends JPanel {
+  final private Game game;
+
+  private Database database;
+  private ColorScheme colorScheme;
+
+  public MenuPanel(Game game) {
+    database = new Database();
+    colorScheme = new ColorScheme();
+    
+    this.game = game;
+    this.setBackground(colorScheme.indigo);
+    
+    BoxLayout boxLayout = new BoxLayout(this, BoxLayout.Y_AXIS);
+    this.setLayout(boxLayout);
+
+    Render();
+  }
+
+  private void Render() {
+    System.out.println("MenuPanel");
+  }
 }
