cfe3ae19a7b26e7305574b86b0f3525550240c15
PunGrumpy <PunGrumpy@users.noreply.github.com>
2023-03-19 01:42:55 +0000

    🎉 code-style(prettier-java): update code style with prettier-java

--- src/utility/ColorScheme.java
+++ src/utility/ColorScheme.java
@@ -4,13 +4,13 @@ import java.awt.Color;
 
 public class ColorScheme {
 
-  public final static Color red = new Color(255, 0, 0);
-  public final static Color green = new Color(0, 255, 0);
-  public final static Color white = new Color(250, 250, 250);
-  public final static Color black = new Color(0, 0, 0);
-  public final static Color gold = new Color(227, 179, 63); // rgb(227, 179, 63)
-  public final static Color indigo = new Color(64, 66, 88); // rgb(64, 66, 88)
-  public final static Color blueGray = new Color(71, 78, 104); // rgb(71, 78, 104)
-  public final static Color ligthBlueGray = new Color(80, 87, 122); // rgb(80, 87, 122)
-  public final static Color brigthBlueGray = new Color(107, 114, 142); // rgb(107, 114, 142)
+  public static final Color red = new Color(255, 0, 0);
+  public static final Color green = new Color(0, 255, 0);
+  public static final Color white = new Color(250, 250, 250);
+  public static final Color black = new Color(0, 0, 0);
+  public static final Color gold = new Color(227, 179, 63); // rgb(227, 179, 63)
+  public static final Color indigo = new Color(64, 66, 88); // rgb(64, 66, 88)
+  public static final Color blueGray = new Color(71, 78, 104); // rgb(71, 78, 104)
+  public static final Color ligthBlueGray = new Color(80, 87, 122); // rgb(80, 87, 122)
+  public static final Color brigthBlueGray = new Color(107, 114, 142); // rgb(107, 114, 142)
 }
